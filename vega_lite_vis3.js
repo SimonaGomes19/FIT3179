44cbc6eaadd7f19cf66c9154decf5644d236596e
SimonaGomes19 <144538903+SimonaGomes19@users.noreply.github.com>
2023-10-11 12:10:59 +0800

    files

---
+++ vega_lite_vis3.js
@@ -0,0 +1,4 @@
+var vg_1 = "map4.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
