4468688fdcef2137daa5902df8ed938bc315b4d3
SimonaGomes19 <144538903+SimonaGomes19@users.noreply.github.com>
2023-10-15 22:53:57 +0800

    files for vis2

--- vega_lite_vis2_2.js
+++ vega_lite_vis2_2.js
@@ -1,4 +1,4 @@
-var vg_1 = "worldMap.vg.json";
-vegaEmbed("#worldMap", vg_1,{rendered:"svg",actions:false}).then(function(result) {
+var vg_1 = "LineChart.vg.json";
+vegaEmbed("#LineChart", vg_1,{rendered:"svg",actions:false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
--- vega_lite_vis2_4.js
+++ vega_lite_vis2_4.js
@@ -1,4 +1,4 @@
-var vg_1 = "worldMap.vg.json";
-vegaEmbed("#worldMap", vg_1,{rendered:"svg",actions:false}).then(function(result) {
+var vg_1 = "BubbleChart.vg.json";
+vegaEmbed("#BubbleChart", vg_1,{rendered:"svg",actions:false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
